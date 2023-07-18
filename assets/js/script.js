156eed5633d22304afea0eb0e5765b5bf5337b32
jmcshan1 <jmcshan01@gmail.com>
2023-07-18 11:05:04 -0500

    added fetch request for weather data, appended data to top section in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 var currentDate = dayjs().format('M/D/YYYY');
 var searchButtonEl = $('#search-button');
 var userCityEl = $('#user-city-name')
+var weatherApiKey = 'cb47f49a6af87f2d9fee75b2e1c52c27';
 
-function getCityWeather(lat,lon){
-    var weatherApi = 'api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=cb47f49a6af87f2d9fee75b2e1c52c27';
+function fetchCityWeatherData(lat,lon){
+    var weatherApi = 'http://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid='+ weatherApiKey;
     console.log(weatherApi);
     fetch(weatherApi)
         .then(function(response){
@@ -11,8 +12,39 @@ function getCityWeather(lat,lon){
         })
         .then(function(data){
             console.log(data);
+            console.log(data.list[0].main.temp);
+            console.log(data.list[0].main.humidity);
+            console.log(data.list[0].wind.speed);
+            var currentTemp = (((data.list[0].main.temp - 273.15) * 9/5) + 32).toFixed(2);
+            var currentWind = data.list[0].main.humidity;
+            var currentHumidity = data.list[0].wind.speed;
 
+            appendCurrentDate(currentTemp, currentWind, currentHumidity);
+            for(var i = 0; i < data.list.length; i + 7){
+                var tempF = (((data.list[i].main.temp - 273.15) * 9/5) + 32).toFixed(2);
+                var temp = tempF.toFixed(2);
+                var wind = data.list[i].main.humidity;
+                var humidity = data.list[i].wind.speed;
+                appendWeatherCard(temp,wind,humidity);
+            }
+            
         })
+}
+
+function appendCurrentDate(temp, wind,humidity){
+    var tempEl = $('#temperature');
+    var windEl = $('#wind-speeds');
+    var humidityEl = $('#humidity');
+
+    tempEl.text(temp + '°F');
+    windEl.text(wind + " MPH");
+    humidityEl.text(humidity + " %");
+}
+
+function appendWeatherCard(temp,wind,humidity){
+    var cardTempEl = $('<p>' + temp + '°F</p>')
+    var cardWindEl = $('<p>' +wind + ' MPH</p>')
+    var cardHumidityEl = $('<p>' + humidity + ' %</p>')
 }
 
 function convertUserCity(city){
@@ -30,7 +62,7 @@ function convertUserCity(city){
             console.log(latitude);
             longitude = data[0].lon;
             console.log(longitude);
-            getCityWeather(latitude,longitude);
+            fetchCityWeatherData(latitude,longitude);
         })
     
         
